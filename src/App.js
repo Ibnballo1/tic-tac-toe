a44e13e63129018157269bdd1cbeb8f25e42c237
Ibnballo1 <belloabdullateef035@gmail.com>
2023-04-09 15:19:55 +0100

    Add winner

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function Board() {
   const [square, setSquare] = useState(Array(9).fill(null))
 
   function handleClick(i) {
-    if(square[i]) {
+    if(square[i] || calculateWinner(square)) {
       return;
     }
     const nextSquare = [...square];
@@ -29,8 +29,18 @@ export default function Board() {
     setSquare(nextSquare);
     setXIsNext(!xIsNext);
   }
+
+  const winner = calculateWinner(square)
+  let status;
+  if (winner) {
+    status = 'Winner is: ' + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={square[0]} onSquareClick={() => handleClick(0)} />
         <Square value={square[1]} onSquareClick={() => handleClick(1)} />
@@ -48,7 +58,33 @@ export default function Board() {
       </div>
     </>
   );
+
+  // Calculate the winner
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 }
+
 
 
 
