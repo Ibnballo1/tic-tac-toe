32513546f5ddef8f2a25427b6357426bbea2c58e
Ibnballo1 <belloabdullateef035@gmail.com>
2023-04-09 15:02:53 +0100

    Update board

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null)
-  const handleClick = () => {
-    setValue('X')
-  }
+function Square(props) {
+  const { value, onSquareClick } = props;
   return (
     <button
       className="square"
-      onClick={handleClick}
+      onClick={onSquareClick}
     >
       {value}
     </button>
@@ -16,26 +13,44 @@ function Square() {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [square, setSquare] = useState(Array(9).fill(null))
+
+  function handleClick(i) {
+    if(square[i]) {
+      return;
+    }
+    const nextSquare = [...square];
+    if (xIsNext) {
+      nextSquare[i] = "X";
+    } else {
+      nextSquare[i] = "O";
+    }
+    setSquare(nextSquare);
+    setXIsNext(!xIsNext);
+  }
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={square[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={square[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={square[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={square[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={square[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={square[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
 }
+
+
 
 
 
